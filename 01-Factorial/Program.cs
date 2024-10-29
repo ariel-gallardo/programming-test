049644f4dd3270708b2cfcf3fd4551a242fb597d
Ariel Gallardo <ariel.gallardo.dev@gmail.com>
2024-10-29 18:51:13 -0300

    factorial-01# solved

---
+++ 01-Factorial/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _01_Factorial
+{
+    internal class Program
+    {
+        private static int Factorial(int num)
+        => num >1 ? num * Factorial(num-1) : num;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Factorial(3));
+            Console.ReadLine();
+        }
+    }
+}
